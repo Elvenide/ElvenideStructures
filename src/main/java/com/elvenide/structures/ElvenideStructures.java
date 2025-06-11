c0b6d6d043cea7921df9572b5566678fd10e5ae7
Cannicide <msffodomainer@gmail.com>
2025-06-11 18:08:19 -0400

    Added config reload command

--- src/main/java/com/elvenide/structures/ElvenideStructures.java
+++ src/main/java/com/elvenide/structures/ElvenideStructures.java
@@ -1,6 +1,9 @@
 package com.elvenide.structures;
 
 import com.elvenide.core.Core;
+import com.elvenide.core.providers.command.SubCommand;
+import com.elvenide.core.providers.command.SubCommandBuilder;
+import com.elvenide.core.providers.command.SubCommandContext;
 import com.elvenide.core.providers.plugin.CorePlugin;
 import com.elvenide.structures.doors.DoorManager;
 import com.elvenide.structures.doors.commands.DoorDeleteCommand;
@@ -10,6 +13,8 @@ import com.elvenide.structures.elevators.commands.ElevatorCreateCommand;
 import com.elvenide.structures.elevators.commands.ElevatorDeleteCommand;
 import com.elvenide.structures.elevators.commands.ElevatorRunCommand;
 import com.elvenide.structures.switches.SwitchListener;
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
 
 public final class ElvenideStructures extends CorePlugin {
 
@@ -29,6 +34,7 @@ public final class ElvenideStructures extends CorePlugin {
         registerListeners(new SwitchListener());
 
         Core.commands.setHeader("<gradient:aqua:dark_aqua>Elvenide Structures");
+        Core.lang.common.setCommandUsagePrefix("<gray>/");
 
         Core.commands.create("elvenidestructures")
             .setAliases("estructures", "estruct")
@@ -46,6 +52,30 @@ public final class ElvenideStructures extends CorePlugin {
                             .addSubCommand(new SlidingDoorCommand());
                     })
                     .addSubCommand(new DoorDeleteCommand());
+            })
+            .addSubCommand(new SubCommand() {
+                @Override
+                public @NotNull String label() {
+                    return "reload";
+                }
+
+                @Override
+                public void setup(@NotNull SubCommandBuilder builder) {
+                    builder
+                        .setDescription("<dark_red>Reloads the plugin configurations in the current world")
+                        .setPermission("elvenidestructures.commands.reload")
+                        .setPlayerOnly();
+                }
+
+                @Override
+                public void executes(@NotNull SubCommandContext context) {
+                    World world = context.player().getWorld();
+                    elevatorManager.getConfiguration(world).reload();
+                    elevatorManager.getStructures(world).reload();
+                    doorManager.getConfiguration(world).reload();
+                    doorManager.getStructures(world).reload();
+                    context.reply("<green>Configurations successfully reloaded in world '{}'!", world.getName());
+                }
             })
             .addHelpSubCommand();
     }
